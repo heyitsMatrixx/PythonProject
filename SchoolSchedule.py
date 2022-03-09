f358b3f580884d8bbef6f7455c3be20f8110ebdf
heyitsMatrixx <90841873+heyitsMatrixx@users.noreply.github.com>
2022-03-09 13:09:33 -0500

    added courses and students

--- SchoolSchedule.py
+++ SchoolSchedule.py
@@ -14,13 +14,30 @@ class Course:
         self.name = name
         self.max_students = max_students
         self.students = []
-        self.req = req
+        self.req = req 
+        # req: [[59.5 (or higher | int), "Class (string)"], Honors Req (bool)]
+        # use "n" if section is blank
     
     def add_students(self, student):
         if len(self.students) < self.max_students:
-            self.students.append(student)
-            return True
+            if self.req[0][0] != "n" and self.grade > req[0][0]:
+                self.students.append(student)
+                return True
+            else:
+                self.students.append(student)
+                return True
         return False
-        
-def addStudent():
-    _name = input()
+
+courses = {
+    "Algebra I":Course("Algebra I", 6, [["n", "n"], False]),
+    "Geometry":Course("Geometry", 5, [[59.5, "Algebra I"], False]),
+    "Algebra II":Course("Algebra II", 3, [[79.5, "Geometry"], True])
+}
+
+courseNames = [
+    "Algebra I", "Geometry", "Algebra II"
+]
+
+students = [
+    Student("Bill", 15, 89, ["Geometry"])
+]
